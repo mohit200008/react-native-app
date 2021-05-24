c30b53847770df57138b13b005cae0b494f144e9
mohit <mohitklamba@gmail.com>
2021-05-24 15:59:22 +0530

    touchable comp

--- App.js
+++ App.js
@@ -1,46 +1,38 @@
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [people, setPeople]= useState([
-    { name: 'shaun', id: '1'},
-    { name: 'yashi', id: '2'},
-    { name: 'isha', id: '3'},
-    { name: 'prachi', id: '4'},
-    { name: 'luigi', id: '5'},
-    { name: 'paagal', id: '6'},
-    { name: 'browser', id: '7'},
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
   ]);
 
-
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id  != id);
+    });
+  };
 
   return (
     <View style={styles.container}>
-     <FlatList
-       numColumns={2} 
-       keyExtractor={(item)=> item.id}
-       data={people}
-       renderItem={({ item })=> (
-        <Text style={styles.item}>{item.name}</Text>
-       )}
-     />
-     
-    
-    
-    {
-    //    <ScrollView>
-      
-    //  {
-    //    people.map(item=> {
-    //      return (
-    //        <View id={item.id}>
-    //          <Text style={styles.item}>{item.name}</Text>
-    //        </View>
-    //      )
-    //    })
-    //  }
-    //  </ScrollView>
-    } 
+
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
     </View>
   );
 }
@@ -48,16 +40,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-    
   },
   item: {
-    padding: 25,
-    backgroundColor: 'crimson',
-    fontSize: 25,
-    margin:25
-
-  }
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
+  },
 });
